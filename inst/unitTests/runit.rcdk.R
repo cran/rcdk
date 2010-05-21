0d88d8f0b03111d0e059c5eb8ae78963a77d35f7
Rajarshi Guha <rajarshi.guha@gmail.com>
2010-05-21 00:00:00 +0000

    version 2.9.23

--- inst/unitTests/runit.rcdk.R
+++ inst/unitTests/runit.rcdk.R
@@ -1,19 +1,36 @@
 test.is.connected <- function()
 {
-  m <- parse.smiles('CCCC')
+  m <- parse.smiles('CCCC')[[1]]
   connected <- is.connected(m)
   checkTrue(connected)
-  m <- parse.smiles('CCCC.CCCC')  
+  m <- parse.smiles('CCCC.CCCC')[[1]]  
   connected <- is.connected(m)
   checkTrue(!connected)  
 }
 
 test.get.largest <- function() {
-  m <- parse.smiles('CCCC')
+  m <- parse.smiles('CCCC')[[1]]
   l <- get.largest.component(m)
   checkEquals(length(get.atoms(l)), 4)
 
-  m <- parse.smiles('CCCC.CCCCCC.CC')
+  m <- parse.smiles('CCCC.CCCCCC.CC')[[1]]
   l <- get.largest.component(m)
   checkEquals(length(get.atoms(l)), 6)  
+}
+
+test.atom.count <- function() {
+  m <- parse.smiles("CCC")[[1]]
+  natom <- get.atom.count(m)
+  checkEquals(natom, 3)
+
+  convert.implicit.to.explicit(m)
+  natom <- get.atom.count(m)
+  checkEquals(natom, 11)  
+}
+
+test.is.neutral <- function() {
+  m <- parse.smiles("CCC")[[1]]
+  checkTrue(is.neutral(m))
+  m <- parse.smiles('[O-]CC')[[1]]
+  checkTrue(!is.neutral(m))
 }
